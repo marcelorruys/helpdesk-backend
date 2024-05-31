15dc765a712eb557a3c4d499bcd25d57b601268c
marcelorruys <ruysmarcelo@gmail.com>
2024-05-31 17:09:52 -0300

    Perfil de desenvolvimento

---
+++ src/main/java/com/marcelo/helpdesk/config/DevConfig.java
@@ -0,0 +1,26 @@
+package com.marcelo.helpdesk.config;
+
+import com.marcelo.helpdesk.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+    @Autowired
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String value;
+
+    @Bean
+    public Boolean instanciaDB(){
+        if (value.equals("create")){
+            this.dbService.instanciaDB();
+        }
+        return false;
+    }
+}
