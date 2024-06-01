9cc59fe2b4129ea36e62726d4c5c4ac1eddfb3b1
marcelorruys <ruysmarcelo@gmail.com>
2024-06-01 08:56:17 -0300

    Protegendo aplicação contra serialização

--- src/main/java/com/marcelo/helpdesk/domain/Tecnico.java
+++ src/main/java/com/marcelo/helpdesk/domain/Tecnico.java
@@ -1,5 +1,6 @@
 package com.marcelo.helpdesk.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.marcelo.helpdesk.domain.enums.Perfil;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
@@ -18,6 +19,7 @@ public class Tecnico extends Pessoa implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "tecnico")
     private List<Chamado> chamados = new ArrayList<>();
 
