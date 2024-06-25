c0c31f70d37e8699f2651a639f72cec2f51ddf7f
marcelorruys <ruysmarcelo@gmail.com>
2024-06-25 13:18:13 -0300

    findAll para Chamado

--- src/main/java/com/marcelo/helpdesk/resources/ChamadoResource.java
+++ src/main/java/com/marcelo/helpdesk/resources/ChamadoResource.java
@@ -2,6 +2,7 @@ package com.marcelo.helpdesk.resources;
 
 import com.marcelo.helpdesk.domain.Chamado;
 import com.marcelo.helpdesk.domain.dtos.ChamadoDTO;
+import com.marcelo.helpdesk.domain.dtos.ClienteDTO;
 import com.marcelo.helpdesk.services.ChamadoService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,15 +12,22 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/chamados")
 public class ChamadoResource {
     @Autowired
     private ChamadoService service;
-
     @GetMapping(value = "/{id}")
     public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id){
         Chamado obj = service.findById(id);
         return ResponseEntity.ok().body(new ChamadoDTO(obj));
+    }
+    @GetMapping
+    public ResponseEntity<List<ChamadoDTO>> findAll(){
+        List<Chamado> list = service.findAll();
+        List<ChamadoDTO> listDTO = list.stream().map(ChamadoDTO::new).toList();
+        return ResponseEntity.ok().body(listDTO);
     }
 }
--- src/main/java/com/marcelo/helpdesk/services/ChamadoService.java
+++ src/main/java/com/marcelo/helpdesk/services/ChamadoService.java
@@ -6,6 +6,7 @@ import com.marcelo.helpdesk.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,6 +18,10 @@ public class ChamadoService {
     public Chamado findById(Integer id){
         Optional<Chamado> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id));
+    }
+
+    public List<Chamado> findAll(){
+        return repository.findAll();
     }
 }
 
