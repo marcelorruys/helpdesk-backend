8b5e8865aae44216d1593fd27e4a82a56c3e9ff9
marcelorruys <ruysmarcelo@gmail.com>
2024-06-27 10:42:59 -0300

    update implementado para chamado

--- src/main/java/com/marcelo/helpdesk/resources/ChamadoResource.java
+++ src/main/java/com/marcelo/helpdesk/resources/ChamadoResource.java
@@ -17,21 +17,30 @@ import java.util.List;
 public class ChamadoResource {
     @Autowired
     private ChamadoService service;
+
     @GetMapping(value = "/{id}")
-    public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id){
+    public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id) {
         Chamado obj = service.findById(id);
         return ResponseEntity.ok().body(new ChamadoDTO(obj));
     }
+
     @GetMapping
-    public ResponseEntity<List<ChamadoDTO>> findAll(){
+    public ResponseEntity<List<ChamadoDTO>> findAll() {
         List<Chamado> list = service.findAll();
         List<ChamadoDTO> listDTO = list.stream().map(ChamadoDTO::new).toList();
         return ResponseEntity.ok().body(listDTO);
     }
+
     @PostMapping
-    public ResponseEntity<ChamadoDTO> create(@Valid @RequestBody ChamadoDTO objDTO){
+    public ResponseEntity<ChamadoDTO> create(@Valid @RequestBody ChamadoDTO objDTO) {
         Chamado obj = service.create(objDTO);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ChamadoDTO> update(@PathVariable Integer id, @Valid @RequestBody ChamadoDTO objDTO) {
+        Chamado newObj = service.update(id, objDTO);
+        return ResponseEntity.ok().body(new ChamadoDTO(newObj));
     }
 }
